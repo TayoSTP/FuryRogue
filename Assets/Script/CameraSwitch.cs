51ecf975597700d57ab41b6e19fa90fef71751ba
dylou1 <157849272+dylou1@users.noreply.github.com>
2026-02-06 15:48:24 +0100

    3C Definitif theoriquement

--- Assets/Script/CameraSwitch.cs
+++ Assets/Script/CameraSwitch.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
-    private Vector3 _newPosition;
+     [SerializeField] GameObject _cameraPosition;
+    [SerializeField] GameObject _cameraObject;
+    bool _triggered = false;
+    public float _SmoothTime = 1f;
+    public Vector3 _velocity = Vector3.zero;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,19 +16,39 @@ public class CameraSwitch : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "RoomDoorTrigger")
+        print(other.name);
+        print(other.tag);
+        if (other.gameObject.CompareTag("Player"))
         {
-            _newPosition = other.GetComponent<NewCameraPointHolder>()._newCameraPoint.transform.position;
+            GameObject player = other.gameObject;
+            if(_cameraObject.GetComponent<CameraBehavior>()._canFollow == false)
+            {
+                _triggered = false;
+                _cameraObject.GetComponent<CameraBehavior>()._canFollow = true;
+                
+            }
+            else
+            {
+                _cameraObject.GetComponent<CameraBehavior>()._canFollow = false;
+                //_cameraObject.transform.position = _cameraPosition.transform.position;
+                _triggered = true;
+            }
+            
             
         }
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_newPosition != null)
+        if (_triggered)
         {
-           // gameObject.transform.position = new Vector3(Mathf.Lerp(gameObject.transform.position, _newPosition, Time.deltaTime);
+            //ector3 pos = _player.transform.position + _offset;
+            _cameraObject.transform.position = Vector3.SmoothDamp(_cameraObject.transform.position, _cameraPosition.transform.position,ref _velocity,_SmoothTime );
+            
+            
         }
     }
 }
