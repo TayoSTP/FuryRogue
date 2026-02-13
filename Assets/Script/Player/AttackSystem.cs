5f73a2fa02556bbc5130cba8c011a10238469f0f
dylou1 <157849272+dylou1@users.noreply.github.com>
2026-02-13 13:43:55 +0100

    add animations

--- Assets/Script/Player/AttackSystem.cs
+++ Assets/Script/Player/AttackSystem.cs
@@ -10,6 +10,12 @@ public class AttackSystem : MonoBehaviour
     private Vector2[] _segments;
     private LineRenderer _lineRenderer;
     private const float TIME_CURVE_ADDITION = 0.5f;
+    private bool canAttack = true;
+    private int attackNumber = 1;
+    private float attackRate = 0.5f;
+    private float lastAttack;
+    bool arrowInHand = false;
+    
     
     
     private int _ammo;
@@ -22,6 +28,8 @@ public class AttackSystem : MonoBehaviour
     [SerializeField] private float  _projectileGravityFromRB;
     [SerializeField] private GameObject _exploPrefab;
     [SerializeField] public int bigAttackChancePourcentage;
+    [SerializeField] Animator _animator;
+    public bool plugIn1;
     
     void Start()
     {
@@ -30,6 +38,8 @@ public class AttackSystem : MonoBehaviour
         
         _lineRenderer = GetComponent<LineRenderer>();
         _lineRenderer.positionCount = _segmentCount;
+        
+        _animator.SetInteger(("AttackNumber"), attackNumber);
     }
 
     // Update is called once per frame
@@ -55,7 +65,19 @@ public class AttackSystem : MonoBehaviour
     {
         if (Time.time > _lastShot + _fireRate && _ammo > 0)
         {
-            Instantiate(_arrowPrefab, _muzzle.transform.position, _muzzle.transform.rotation);
+            if (value.isPressed)
+            {
+                arrowInHand = true;
+                Instantiate(_arrowPrefab, _muzzle.transform.position, _muzzle.transform.rotation);
+                _animator.SetBool("ArrowInHand", arrowInHand);
+            }
+            else if (arrowInHand && !value.isPressed)
+            {
+                arrowInHand = false;
+                _animator.SetBool("ArrowInHand", arrowInHand);
+            }
+        
+            
             _lastShot = Time.time;
             _ammo--;
         }
@@ -63,33 +85,54 @@ public class AttackSystem : MonoBehaviour
 
     void OnLightAttack(InputValue value)
     {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        Vector3 castOrigin = new Vector3(transform.position.x, transform.position.y +1, transform.position.z);
-        if (Physics.Raycast(castOrigin, fwd, out RaycastHit hit, 1.5f))
+        if (canAttack)
         {
-            if (hit.collider.gameObject.CompareTag("Ennemy"))
+            if (attackNumber == 1 && lastAttack <= 1)
             {
-                if ((100 * Random.Range(0, 100))/100 <= bigAttackChancePourcentage)
-                {
-                    Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
-                    rb.AddForce(gameObject.transform.forward * 150);
-                    hit.collider.gameObject.GetComponent<AI_Stats>().looseHealth(_attackDamage+10);
-                    _hitsnumber = 0;
-                    Instantiate(_exploPrefab, hit.point, Quaternion.identity);
-                    print("boom");
-                }
-                else
-                {
-                    Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
-                    rb.AddForce(gameObject.transform.forward * 100);
-                    hit.collider.gameObject.GetComponent<AI_Stats>().looseHealth(_attackDamage);
-                    _hitsnumber++;
-                }
-                
+                attackNumber = 2;
+                _animator.SetInteger(("AttackNumber"), attackNumber);
             }
+            else
+            {
+                attackNumber = 1;
+                _animator.SetInteger(("AttackNumber"), attackNumber);
+            }
+            _animator.SetTrigger("Punch");
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            Vector3 castOrigin = new Vector3(transform.position.x, transform.position.y +1, transform.position.z);
+            if (Physics.Raycast(castOrigin, fwd, out RaycastHit hit, 1.5f))
+            {
+            
+                if (hit.collider.gameObject.CompareTag("Ennemy"))
+                {
+                    if ((100 * Random.Range(0, 100))/100 <= bigAttackChancePourcentage && plugIn1)
+                    {
+                        Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+                        rb.AddForce(gameObject.transform.forward * 150);
+                        hit.collider.gameObject.GetComponent<AI_Stats>().looseHealth(_attackDamage+10);
+                        _hitsnumber = 0;
+                        Instantiate(_exploPrefab, hit.point, Quaternion.identity);
+                        print("boom");
+                    }
+                    else
+                    {
+                        Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+                        rb.AddForce(gameObject.transform.forward * 100);
+                        hit.collider.gameObject.GetComponent<AI_Stats>().looseHealth(_attackDamage);
+                        _hitsnumber++;
+                    }
+                
+                }
+            }
+            Invoke("ResetAttack", attackRate);
         }
         
-            
+        
+    }
+
+    void ResetAttack()
+    {
+        canAttack = true;
     }
     private void OnDrawGizmos() 
     {
--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -4,13 +4,25 @@ using System.Collections.Generic;
 using System.Collections;
 using Unity.Mathematics;
 using UnityEngine.InputSystem;
+using UnityEngine.Animations;
+using UnityEngine.PlayerLoop;
 
 public class TestPlayerMovement : MonoBehaviour
 {
+    
+    [Header("Run")]
     [SerializeField] private float _acceleration;
+    [SerializeField] private float _accelAmount;
     [SerializeField] private float _deceleration;
+    [SerializeField] private float _decelAmount;
     [SerializeField] private float _maxSpeed;
+    
+    
+    [Range(0.01f, 1)] [SerializeField] private float _accelInAir;
+    [Range(0.01f, 1)] [SerializeField] private float _decelInAir;
     [SerializeField] private float _jumpForce;
+
+    private bool _conserveMomentum;
     [SerializeField] private float _gravityScale;
     //[SerializeField] private float _fallingGravityScale;
     [SerializeField] public float _dashForce;
@@ -23,29 +35,54 @@ public class TestPlayerMovement : MonoBehaviour
     private float pRot;
     public bool _followCamera = true;
     Vector2 _movement;
+
+    private bool isJumping;
+    bool isJumpFalling = false;
+
+    private float targetSpeed;
     
     
+    [SerializeField] Animator _animator;
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _currentGravityScale = _gravityScale;
+        //_animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnValidate()
+    {
+        _accelAmount = (50 * _acceleration) / _maxSpeed;
+        _decelAmount = (50 * _deceleration) / _maxSpeed;
+
+        #region Variable Ranges
+        _acceleration = Mathf.Clamp(_acceleration, 0.01f, _maxSpeed);
+        _deceleration = Mathf.Clamp(_deceleration, 0.01f, _maxSpeed);
+        #endregion
     }
 
     void OnMove(InputValue value)
     {
         _movement = value.Get<Vector2>();
+        _animator.SetFloat("MovementValue", _movement.x);
     }
 
     void OnJump(InputValue value)
     {
         if (_grounded)
         {
-            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            if (_movement != Vector2.zero)
+            _animator.SetTrigger("Jump");
+            float jumpForce = _jumpForce;
+            if (_rb.linearVelocity.y < 0)
             {
-                _rb.AddForce(Vector3.forward * _movement * 6.25f, ForceMode.Impulse);
+                jumpForce -= _rb.linearVelocity.y;
             }
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            /*if (_movement != Vector2.zero)
+            {
+                _rb.AddForce(Vector3.forward * _movement * 6.25f, ForceMode.Impulse);   
+            }*/
             
             //_rb.linearVelocity += new Vector3(0, _jumpForce, 0);
         }
@@ -57,10 +94,10 @@ public class TestPlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        
+        targetSpeed = _movement.magnitude;
         //Grounded Check
         
-        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1.8f))
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 1.1f))
         { 
             _grounded = true;   
         }
@@ -78,13 +115,30 @@ public class TestPlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         _canDash = true;
     }
-    
     private void FixedUpdate()
     {
-        //Vector3 movement = new Vector3(_movement.x, _rb.linearVelocity.y, 0);
-        //if (movement  != Vector3.zero)
-        //{
-            _rb.AddForce(new Vector3(_movement.x,0,0) * _acceleration, ForceMode.Force);
+        
+        _animator.SetBool("IsWalking", _movement.x != 0);
+        Run();
+        if (_rb.linearVelocity.y < 0)
+        {
+            _rb.AddForce((Vector3.down * (_gravityScale + 1.5f)), ForceMode.Acceleration);
+        }
+        else
+        {
+            _rb.AddForce((Vector3.down * _gravityScale), ForceMode.Acceleration);
+        }
+        //movement
+        /*float speedDif = targetSpeed - _rb.linearVelocity.x;
+        float movement = speedDif * _acceleration;
+        float accelRate;
+        if (_grounded)
+        {
+            accelRate = (Mathf.Abs(targetSpeed) > 0.1f) ? _acceleration : _deceleration;
+        }
+        _animator.SetFloat("Movement", _movement.x);
+            //_rb.AddForce(new Vector3(_movement.x,0,0) * _acceleration, ForceMode.Force);
+            _rb.AddForce(Vector3.right * movement, ForceMode.Force);
             if (_rb.linearVelocity.magnitude > _maxSpeed)
             {
                 _rb.linearVelocity = new Vector3((_rb.linearVelocity.normalized.x * _maxSpeed), _rb.linearVelocity.y);
@@ -94,7 +148,7 @@ public class TestPlayerMovement : MonoBehaviour
         //{
         //   _rb.AddForce(_movement * -_deceleration, ForceMode.Force);
        // }
-        
+        */
        if (_movement.x > 0.8f )
        {
            pRot = 90;
@@ -108,7 +162,45 @@ public class TestPlayerMovement : MonoBehaviour
            pRot = -90;
        }
        gameObject.transform.localRotation = Quaternion.Euler(0, pRot, 0);
-        _rb.AddForce((Vector3.down * _gravityScale), ForceMode.Force);
+       // _rb.AddForce((Vector3.down * _gravityScale), ForceMode.Force);
         
+    }
+
+    void Run()
+    {
+        float targetSpeed = _movement.x * _maxSpeed;
+
+        #region Calculate AccelRate
+        float accelRate;
+
+        if (_grounded)
+        {
+            accelRate = (Mathf.Abs(targetSpeed) > 0.1f) ? _accelAmount : _decelAmount;
+        }
+        else
+        {
+           // accelRate = (Mathf.Abs(targetSpeed) > 0.1f) ? _accelAmount * _accelInAir : _decelAmount * _decelInAir;
+           accelRate = (Mathf.Abs(targetSpeed) > 0.1f) ? _accelAmount : _decelAmount;
+        }
+        #endregion
+        
+        {
+            
+        }
+        
+        #region Conserve Momentum
+
+        if (_conserveMomentum && Mathf.Abs(_rb.linearVelocity.x) > Mathf.Abs(targetSpeed) &&
+            Mathf.Sign(_rb.linearVelocity.x) == Mathf.Sign(targetSpeed))
+        {
+            accelRate = 0;
+        }
+        #endregion
+        
+        float speedDif = targetSpeed - _rb.linearVelocity.x;
+        
+        float movement = speedDif * accelRate;
+        
+        _rb.AddForce(Vector3.right * movement, ForceMode.Force);
     }
 }
