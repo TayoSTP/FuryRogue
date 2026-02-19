896834610dd1c0e129b5e3e3189d70aba9f62ec4
Matthias.G <matthias.gimenez.mg@gmail.com>
2026-02-19 14:21:06 +0100

    [SoundDesign] ça marche

--- Assets/Script/SFX/AmbianceSound.cs
+++ Assets/Script/SFX/AmbianceSound.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
@@ -5,14 +6,37 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     public Collider Area;                       // The area of the sound
     public GameObject Player;                   // The object to track
-
+    public AudioSource Audio;
+    private bool canFollow;
     void Update()
     {
-        //Locate closest point on the collider to the player 
-        Vector3 closestPoint = Area.ClosestPoint(Player.transform.position);
-        // Set position to closet point to the Player
-        transform.position = closestPoint;
+        /*if (canFollow)
+        {
+            //Locate closest point on the collider to the player 
+                    Vector3 closestPoint = Area.ClosestPoint(Player.transform.position);
+                    // Set position to closet point to the Player
+                    Audio.transform.position = closestPoint;
+                    
+        }*/
+        
         
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            Audio.Play();
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Audio.Stop();
+        }
+    }
 }
